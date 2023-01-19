d245f350ec0015277344f8eb7515a9267d2d0858
JefersonLC <c.jeferson.lc@gmail.com>
2023-01-18 19:06:06 -0500

    modified LoginForm.js

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,12 +1,29 @@
 'use client';
 
-import { ErrorMessage, Field, Form, Formik } from 'formik';
+import { Form, Formik } from 'formik';
+import { faLock, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { loginSchema } from '../schemas/authSchemas';
+import AuthInput from './AuthInput';
 
 const loginValues = {
   email: '',
   password: '',
 };
+
+const loginInputs = [
+  {
+    icon: faEnvelope,
+    name: 'email',
+    type: 'text',
+    placeholder: 'Email',
+  },
+  {
+    icon: faLock,
+    name: 'password',
+    type: 'password',
+    placeholder: 'Password',
+  },
+];
 
 export default function LoginForm() {
   return (
@@ -17,16 +34,23 @@ export default function LoginForm() {
         console.log(values);
       }}
     >
-      <Form>
-        <div>
-          <Field name="email" />
-          <ErrorMessage name="email" component="span" />
+      <Form className="form">
+        {loginInputs.map((input, index) => (
+          <AuthInput
+            key={index}
+            icon={input.icon}
+            name={input.name}
+            type={input.name}
+            placeholder={input.placeholder}
+          />
+        ))}
+        <div className="form-control form-help">
+          <p>Yo don't have an account?</p>
+          <p>Don't remember your password?</p>
         </div>
-        <div>
-          <Field name="password" type="password" />
-          <ErrorMessage name="password" component="span" />
-        </div>
-        <button type="submit">Login</button>
+        <button type="submit" className="form-button">
+          Login
+        </button>
       </Form>
     </Formik>
   );
