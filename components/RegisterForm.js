d11952bcc57bc56e77cc35a0c2f4ba8c651c2a32
JefersonLC <c.jeferson.lc@gmail.com>
2023-01-18 19:06:36 -0500

    added RegisterForm.js

---
+++ components/RegisterForm.js
@@ -0,0 +1,82 @@
+'use client';
+
+import { Form, Formik } from 'formik';
+import {
+  faUser,
+  faUserPlus,
+  faLock,
+  faEnvelope,
+} from '@fortawesome/free-solid-svg-icons';
+import { registerSchema } from '../schemas/authSchemas';
+import AuthInput from './AuthInput';
+
+const registerValues = {
+  name: '',
+  lastname: '',
+  email: '',
+  password: '',
+  repeat_password: '',
+};
+
+const registerInputs = [
+  {
+    icon: faUser,
+    name: 'name',
+    type: 'text',
+    placeholder: 'Name',
+  },
+  {
+    icon: faUserPlus,
+    name: 'lastname',
+    type: 'text',
+    placeholder: 'Lastname',
+  },
+  {
+    icon: faEnvelope,
+    name: 'email',
+    type: 'text',
+    placeholder: 'Email',
+  },
+  {
+    icon: faLock,
+    name: 'password',
+    type: 'password',
+    placeholder: 'Password',
+  },
+  {
+    icon: faLock,
+    name: 'repeat_password',
+    type: 'password',
+    placeholder: 'Confirm password',
+  },
+];
+
+export default function RegisterForm() {
+  return (
+    <Formik
+      initialValues={registerValues}
+      validationSchema={registerSchema}
+      onSubmit={(values) => {
+        console.log(values);
+      }}
+    >
+      <Form className="form">
+        {registerInputs.map((input, index) => (
+          <AuthInput
+            key={index}
+            icon={input.icon}
+            name={input.name}
+            type={input.type}
+            placeholder={input.placeholder}
+          />
+        ))}
+        <div className="form-control form-help">
+          <p>Are you already registered?</p>
+        </div>
+        <button type="submit" className="form-button">
+          Register
+        </button>
+      </Form>
+    </Formik>
+  );
+}
