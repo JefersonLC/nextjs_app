4a957e42631c466d2baa8e4af7138b98e5ca81ac
JefersonLC <c.jeferson.lc@gmail.com>
2023-01-19 21:16:26 -0500

    modified layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import { Roboto } from '@next/font/google';
 import Link from 'next/link';
 import NavContent from '../components/NavContent';
 import '../styles/globals.css';
-import '../styles/RootLayout.css'
+import '../styles/RootLayout.css';
 
 const roboto = Roboto({
   weight: ['400', '700'],
@@ -15,16 +15,17 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={roboto.className}>
       <head>
-        <title>APP NEXT 13</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body>
-        <nav className="nav">
-          <div className="nav-container">
-            <Link href="/" className='nav-brand'>STORE</Link>
+        <header className="nav">
+          <nav className="nav-container">
+            <Link href="/" className="nav-brand">
+              STORE
+            </Link>
             <NavContent />
-          </div>
-        </nav>
+          </nav>
+        </header>
         {children}
       </body>
     </html>
