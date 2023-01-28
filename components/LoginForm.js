5dc867024e39bb47f38ad5f763e113368f36532b
JefersonLC <c.jeferson.lc@gmail.com>
2023-01-27 21:59:31 -0500

    modified LoginForm component

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,12 +1,11 @@
 'use client';
 
-import { Form, Formik } from 'formik';
+import { Form, Formik, Field } from 'formik';
 import { faLock, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { loginSchema } from '../schemas/authSchemas';
 import AuthInput from './AuthInput';
 import { useLoginStore } from '../store/loginStore';
 import { loginUser } from '../services/api';
-import Alert from './Alert';
 
 const loginValues = {
   email: '',
@@ -19,52 +18,61 @@ const loginInputs = [
     name: 'email',
     type: 'text',
     placeholder: 'Email',
+    isField: true,
   },
   {
     icon: faLock,
     name: 'password',
     type: 'password',
     placeholder: 'Password',
+    isField: true,
   },
 ];
 
 export default function LoginForm() {
-  const { error, setError } = useLoginStore();
+  const { setError } = useLoginStore();
 
   return (
-    <>
-      {error && <Alert message={error} />}
-      <Formik
-        initialValues={loginValues}
-        validationSchema={loginSchema}
-        onSubmit={async (values) => {
-          try {
-            await loginUser(values);
-          } catch (err) {
-            const message = err.toString();
-            setError(message);
-          }
-        }}
-      >
-        <Form className="form">
-          {loginInputs.map((input, index) => (
-            <AuthInput
-              key={index}
-              icon={input.icon}
-              name={input.name}
-              type={input.name}
-              placeholder={input.placeholder}
-            />
-          ))}
-          <div className="form-control form-help">
-            <p>Yo don't have an account?</p>
-            <p>Don't remember your password?</p>
-          </div>
-          <button type="submit" className="form-button">
-            Login
-          </button>
-        </Form>
-      </Formik>
-    </>
+    <Formik
+      initialValues={loginValues}
+      validationSchema={loginSchema}
+      onSubmit={async (values) => {
+        try {
+          await loginUser(values);
+        } catch (err) {
+          const message = err.toString();
+          setError(message);
+        }
+      }}
+    >
+      <Form className="form">
+        {loginInputs.map((input, index) => (
+          <AuthInput key={index} icon={input.icon} name={input.name}>
+            {input.isField ? (
+              <Field
+                name={input.name}
+                type={input.type}
+                placeholder={input.placeholder}
+                className="input"
+              />
+            ) : (
+              <input
+                name={input.name}
+                type={input.type}
+                placeholder={input.placeholder}
+                className="input"
+              />
+            )}
+          </AuthInput>
+        ))}
+        <div className="form-control form-help">
+          <p>Yo don't have an account?</p>
+          <p>Don't remember your password?</p>
+        </div>
+        <button type="submit" className="form-button">
+          Login
+        </button>
+      </Form>
+    </Formik>
   );
 }
