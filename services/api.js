044ed92ef6341c18604da5348ca1b62095275424
JefersonLC <c.jeferson.lc@gmail.com>
2023-01-27 22:00:30 -0500

    added getProducts function

--- services/api.js
+++ services/api.js
@@ -1,17 +1,27 @@
+const path = 'http://localhost:3001/api/store';
+
+export async function getProducts() {
+  const res = await fetch(`${path}/products`, {
+    next: { revalidate: 120 },
+  });
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+  const products = await res.json();
+  return products;
+}
+
 export async function loginUser(user) {
   const bodyContent = JSON.stringify(user);
-  const response = await fetch(
-    'https://ts-api-kqij.onrender.com/api/store/login',
-    {
-      method: 'POST',
-      body: bodyContent,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    }
-  );
-  const data = await response.json();
-  if (!response.ok) {
+  const res = await fetch(`${path}/login`, {
+    method: 'POST',
+    body: bodyContent,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  const data = await res.json();
+  if (!res.ok) {
     throw new Error(data.message);
   }
   console.log(data);
