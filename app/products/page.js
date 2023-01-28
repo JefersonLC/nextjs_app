6658ce12054c0653429e6a704bb119cde5e63546
JefersonLC <c.jeferson.lc@gmail.com>
2023-01-27 21:56:50 -0500

    modified ProductsPage component

--- app/products/page.js
+++ app/products/page.js
@@ -1,30 +1,19 @@
-import Image from 'next/image'
 import '../../styles/ProductsPage.css';
-
-async function getProducts() {
-  const res = await fetch(
-    'https://ts-api-kqij.onrender.com/api/store/products',
-    { next: { revalidate: 10 } }
-  );
-  if (!res.ok) {
-    throw new Error('Failed to fetch data');
-  }
-  const products = await res.json();
-  return products;
-}
+import { getProducts } from '../../services/api';
 
 export default async function ProductsPage() {
   const products = await getProducts();
+
   return (
     <div className="cards">
       {products.map((product) => (
-        <div key={product.id} className='.card'>
+        <div key={product.id} className=".card">
           <div>
-            <Image src="/dasdsadsd" width={100} height={100} alt="xd"/>
+            <img src={product.photo} width={100} height={100} alt="xd" />
           </div>
-          <div className='card-body'>
-            <h5 className='card-body-title'>{product.name}</h5>
-            <div className='card-body-info'>
+          <div className="card-body">
+            <h5 className="card-body-title">{product.name}</h5>
+            <div className="card-body-info">
               <span>Stock: {product.stock}</span>
               <span>Precio: S/. {product.price}</span>
             </div>
